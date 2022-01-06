a035a0998cd65405206bff248687c58c7c455b56
Gavin Grochowski <grochowskigavin@gmail.com>
2022-01-06 14:35:19 -0800

    Multi Page (url ) Support

--- layout/components/sidebar.py
+++ layout/components/sidebar.py
@@ -3,8 +3,7 @@ from dash import html
 
 SIDEBAR = dbc.Col(
     class_name="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark",
-    style={"width": "280px"},
-    width=280,
+    style={"height": "100vh"},
     children=[
         html.A(
             className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none",
@@ -12,7 +11,8 @@ SIDEBAR = dbc.Col(
             children=[
                 html.Img(className="bi me-2", height=44, src="assets/cd_icon.png"),
                 html.Span(
-                    className="fs-4 fw-bold lh-1", children="Your Favourite Albums 2021"
+                    className="fs-4 fw-bold lh-1",
+                    children="Your Favourite \nAlbums 2021",
                 ),
             ],
         ),
@@ -23,8 +23,27 @@ SIDEBAR = dbc.Col(
                 dbc.NavItem(
                     children=[
                         dbc.NavLink(
-                            active=True,
-                            href="#",
+                            active="exact",
+                            href="/home",
+                            children=[
+                                html.Img(
+                                    className="bi me-2",
+                                    height=16,
+                                    width=16,
+                                    src="assets/cd_icon.png",
+                                ),
+                                html.Span(
+                                    className="fs-6 fw-light p-3", children="Home"
+                                ),
+                            ],
+                        )
+                    ]
+                ),
+                dbc.NavItem(
+                    children=[
+                        dbc.NavLink(
+                            active="exact",
+                            href="/top_10",
                             children=[
                                 html.Img(
                                     className="bi me-2",
@@ -42,8 +61,8 @@ SIDEBAR = dbc.Col(
                 dbc.NavItem(
                     children=[
                         dbc.NavLink(
-                            active="partial",
-                            href="#",
+                            active="exact",
+                            href="/discover",
                             children=[
                                 html.Img(
                                     className="bi me-2",
@@ -52,7 +71,7 @@ SIDEBAR = dbc.Col(
                                     src="assets/cd_icon.png",
                                 ),
                                 html.Span(
-                                    className="fs-6 fw-light p-3", children="Discovery"
+                                    className="fs-6 fw-light p-3", children="Discover"
                                 ),
                             ],
                         )
@@ -61,8 +80,8 @@ SIDEBAR = dbc.Col(
                 dbc.NavItem(
                     children=[
                         dbc.NavLink(
-                            active="partial",
-                            href="#",
+                            active="exact",
+                            href="/compare",
                             children=[
                                 html.Img(
                                     className="bi me-2",
@@ -80,7 +99,16 @@ SIDEBAR = dbc.Col(
             ],
         ),
         html.Hr(),
-        html.Span(className="fs-6 fw-light p-2", children="Album Search"),
+        dbc.Label("Rank By:"),
+        dbc.RadioItems(
+            options=[
+                {"label": "Album Score", "value": 1},
+                {"label": "Album Submissions", "value": 2},
+            ],
+            value=1,
+            id="rank-by",
+        ),
+        dbc.Label("Album Search:", class_name="mt-3"),
         dbc.Input(
             className="mb-3 p-2",
             placeholder="Enter Album Name...",
--- layout/layout.py
+++ layout/layout.py
@@ -1,31 +1,65 @@
 import dash
 from dash import dcc
 from dash import html
+from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
 
+from app import app
 from .components.banner import BANNER
 from .components.sidebar import SIDEBAR
 from .components.viz.top_10_album_score import TOP_10_ALBUM_SCORE_FIG
 
 
-layout1 = html.Div(
-    [
+# APP LAYOUT
+layout = html.Div(
+    children=[
+        dcc.Location(id="url", refresh=False),
         dbc.Container(
             fluid=True,
+            style={"min-height": "100vh", "height": "100vh"},
             children=[
                 dbc.Row(
                     children=[
                         SIDEBAR,
                         dbc.Col(
-                            class_name="col-md-9",
-                            children=[
-                                dcc.Graph(figure=TOP_10_ALBUM_SCORE_FIG),
-                                dcc.Graph(figure=TOP_10_ALBUM_SCORE_FIG),
-                            ],
+                            className="col-md-9",
+                            id="page-content",
                         ),
                     ]
                 ),
             ],
         ),
-    ]
+    ],
 )
+
+# PAGES
+
+home_page = "HOME"
+top_10_page = [
+    dcc.Graph(figure=TOP_10_ALBUM_SCORE_FIG),
+    dcc.Graph(figure=TOP_10_ALBUM_SCORE_FIG),
+]
+discover_page = "DISCOVER"
+compare_page = "COMPARE"
+
+# PAGE INDEX
+
+
+@app.callback(Output("page-content", "children"), Input("url", "pathname"))
+def display_page(pathname):
+    if pathname == "/":
+        return home_page
+    elif pathname == "/home":
+        return home_page
+    elif pathname == "/top_10":
+        return top_10_page
+    elif pathname == "/discover":
+        return discover_page
+    elif pathname == "/compare":
+        return compare_page
+    else:
+        return "404"
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
